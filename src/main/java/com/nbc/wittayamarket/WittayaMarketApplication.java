858e8c3a30de5d21781307e417fdf2f7b1708f9b
superpaulza <18383834+superpaulza@users.noreply.github.com>
2021-11-27 16:13:29 +0700

    test

--- src/main/java/com/nbc/wittayamarket/WittayaMarketApplication.java
+++ src/main/java/com/nbc/wittayamarket/WittayaMarketApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WittayaMarketApplication {
 
     public static void main(String[] args) {
+        System.out.println("Helloworld!");
         SpringApplication.run(WittayaMarketApplication.class, args);
     }
 }
